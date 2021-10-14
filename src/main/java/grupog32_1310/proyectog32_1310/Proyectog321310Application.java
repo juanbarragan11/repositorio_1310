fafaafc4a7ca86b7746fd6d55f0907c2e569014c
Juan Barragan <juan.barragan.mt@correo.usa.edu.co>
2021-10-13 21:35:51 -0500

    Prueba Springboot

---
+++ src/main/java/grupog32_1310/proyectog32_1310/Proyectog321310Application.java
@@ -0,0 +1,13 @@
+package grupog32_1310.proyectog32_1310;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyectog321310Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyectog321310Application.class, args);
+	}
+
+}
---
+++ src/test/java/grupog32_1310/proyectog32_1310/Proyectog321310ApplicationTests.java
@@ -0,0 +1,13 @@
+package grupog32_1310.proyectog32_1310;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyectog321310ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
